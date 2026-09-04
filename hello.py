4200d170b3984a3d13b7cfeea76192b512ed7a4d
Jodie <jodieywy@gmail.com>
2026-09-04 14:36:24 +0800

    Test

--- hello.py
+++ hello.py
@@ -2,3 +2,12 @@ print("=================================")
 print("Welcome here")
 print("My first post!")
 print("=================================")
+
+
+username = 'ihihi'
+bio = 'yh'
+followers = 1000 
+
+print('Username : ', username)
+print('Bio : ', bio)
+print('Followers : ', followers)
