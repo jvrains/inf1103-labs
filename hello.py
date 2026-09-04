fae2ae4c87a28097448f7dafa242089f4fe2cd79
Jodie <jodieywy@gmail.com>
2026-09-04 14:43:42 +0800

    activity 4

--- hello.py
+++ hello.py
@@ -1,18 +1,10 @@
-print("=================================")
-print("Welcome here")
-print("My first post!")
-print("=================================")
+username = input('Enter username: ')
+age = int(input('Enter age: '))
+category = input('Enter Content Category: ')
 
+print("\nInstagram Profile")
+print("=====================")
+print("Username: " + username)
+print("Age: " + age)
+print("Category: " + category)
 
-username = 'ihihi'
-bio = 'yh'
-followers = 100
-
-followers += 50
-print('Day 1 : ', followers)
-
-followers += 20
-print('Day 2 : ', followers)
-
-followers += 10
-print('Day 3 : ', followers)
