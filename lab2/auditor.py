ab8547d176097e583026908c4a3978ca10ab16ed
Jodie <2605387@sit.singaporetech.edu.sg>
2026-09-11 02:09:00 +0800

    part 8

--- lab2/auditor.py
+++ lab2/auditor.py
@@ -1,19 +1,24 @@
 # lab 2
 #1
 i = 0
-a=""
-#2,3,4,5,6
-while a != "quit":
+failure = 0
+#2,3,4,5,6,8
+while True:
     add = input("Please enter a stock quantity, or enter 'quit' to exit: ")
     if add=='quit':
         break
     elif add.lstrip('-').isdigit():
         num = int(add)
-        if num < 0:\
+        if num < 0:
+            failure+=1
             print("Please enter a positive integer.")
         else:
             i+=int(num)
     else:
+        failure+=1
         print("Please enter an integer value.")
         continue
-        
+
+ #8
+print(f"Total Units Processed: {i}") 
+print(f"Number of Failed/Rejected Entries: {failure}")
