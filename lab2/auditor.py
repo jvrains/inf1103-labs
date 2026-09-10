eb9738948fea245a6e26ef6735b06c5cdd429f54
Jodie <2605387@sit.singaporetech.edu.sg>
2026-09-11 02:02:13 +0800

    part 2/3/4/5

--- lab2/auditor.py
+++ lab2/auditor.py
@@ -1,3 +1,19 @@
 # lab 2
 #1
 i = 0
+a=""
+#2,3,4,5,6
+while a != "quit":
+    add = input("Please enter a stock quantity, or enter 'quit' to exit: ")
+    if add=='quit':
+        break
+    elif add.lstrip('-').isdigit():
+        num = int(add)
+        if num < 0:\
+            print("Please enter a positive integer.")
+        else:
+            i+=int(num)
+    else:
+        print("Please enter an integer value.")
+        continue
+        
