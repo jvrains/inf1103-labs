039ad28e6c111357d9b8cfae02fc3338a061b350
Jodie <2605387@sit.singaporetech.edu.sg>
2026-09-21 01:45:46 +0800

    fixed tax

--- lab3/modular_auditor.py
+++ lab3/modular_auditor.py
@@ -13,7 +13,7 @@ def get_valid_input():
         print("Please enter an integer value.")
         return None
 
-    if num < 0:
+    if num <= 0:
         print("Please enter a positive integer.")
         return None
 
@@ -27,17 +27,17 @@ def process_delivery(current_total, new_value):
 def calculate_tax(amount):
     return amount*taxrate
 
-def generate_report(total_units, failed_attempts, total_cost, tax):
+def generate_report(total_units, failed_attempts, delivery):
     print(f"Total Units Processed: {total_units}")
     print(f"Number of Failed/Rejected Entries: {failed_attempts}")
-    print(f"Delivery Tax: ${tax}")
-    print(f"Total Cost of Delivery (after Tax): ${total_cost+tax}")
+    print(f"Number of Deliveries: {delivery}")
     return None
 
 failure = 0
 quantity = 0
 totalcost = 0
 tax = 0
+deliveries = 0
 
 while True:
     entry = get_valid_input()
@@ -48,13 +48,12 @@ while True:
         failure += 1
         continue
     quantity, totalcost = process_delivery(quantity, entry)
+    deliveries += 1
+    tax = calculate_tax(totalcost)
     if quantity > inventorylimit:
         print("Your total inventory has exceeded 500!")
         break
-    tax = calculate_tax(totalcost)
-
-generate_report(quantity, failure, totalcost, tax)
-
+    print(f"Delivery Tax: ${tax:.2f}")
+    print(f"Total Cost of Delivery (after Tax): ${totalcost+tax:.2f}")
     
-
-
+generate_report(quantity, failure, deliveries)
