6d14835cfb6a55a312d488f29c30bfa7dcb64c6b
Jodie <2605387@sit.singaporetech.edu.sg>
2026-09-21 01:26:26 +0800

    test final

--- lab3/modular_auditor.py
+++ lab3/modular_auditor.py
@@ -27,12 +27,18 @@ def process_delivery(current_total, new_value):
 def calculate_tax(amount):
     return amount*taxrate
 
-def generate_report(total_units, failed_attempts):
+def generate_report(total_units, failed_attempts, total_cost, tax):
     print(f"Total Units Processed: {total_units}")
     print(f"Number of Failed/Rejected Entries: {failed_attempts}")
+    print(f"Delivery Tax: ${tax}")
+    print(totalcost)
+    print(f"Total Cost of Delivery (after Tax): ${total_cost+tax}")
     return None
 
 failure = 0
+quantity = 0
+totalcost = 0
+tax = 0
 
 while True:
     entry = get_valid_input()
@@ -42,8 +48,14 @@ while True:
     elif entry == None:
         failure += 1
         continue
-    elif entry > inventorylimit:
+    quantity, totalcost = process_delivery(quantity, entry)
+    if quantity > inventorylimit:
         print("Your total inventory has exceeded 500!")
         break
+    tax = calculate_tax(totalcost)
+
+generate_report(quantity, failure, totalcost, tax)
+
+    
 
 
